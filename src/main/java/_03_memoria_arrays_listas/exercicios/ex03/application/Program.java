68918fa8f81d8916c603b41badd9a4f9be20f8e0
Alfredo Lopes <Alfredo.Lopes.TI@gmail.com>
2026-08-15 18:51:34 -0300

    atualizando exercícios

--- src/main/java/_03_memoria_arrays_listas/exercicios/ex03/application/Program.java
+++ src/main/java/_03_memoria_arrays_listas/exercicios/ex03/application/Program.java
@@ -1,12 +1,31 @@
 package _03_memoria_arrays_listas.exercicios.ex03.application;
 
 import java.util.Scanner;
+import java.util.Locale;
 
 public class Program {
 
     public static void main(String[] args) {
-        
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] mat = new int[n][n];
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < n; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.println(mat[i][i] + "");
+
+        }
+
         sc.close();
+
     }
 }
